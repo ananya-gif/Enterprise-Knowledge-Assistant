3011a8645f1570050187b58ad3c5b8f1a9ceb26f
ananya <ananyacgangadhar@gmail.com>
2026-08-12 20:56:14 +0530

    Initial ASP.NET Core API setup

---
+++ backend/EnterpriseKnowledgeAssistant.Api/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseKnowledgeAssistant.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            application = "Enterprise Knowledge Assistant"
+        });
+    }
+}
